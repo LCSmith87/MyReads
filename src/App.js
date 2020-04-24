cc70eb8ab20a2a937d0e98ca1be79181d1113d59
LCSmith87 <LCSmith87@gmail.com>
2020-04-24 11:05:17 -0500

    added section component to app and categories data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
+import Section from './Section/Section.js';
+
+const data = {
+  categories: [
+    {
+      id: 1,
+      name: 'Currently Reading'
+    },
+    {
+      id: 2,
+      name: 'Want to Read'
+    },
+    {
+      id: 3,
+      name: 'Read'
+    }
+  ]
+};
 
 function App() {
   return (
     <div className="App">
         <div className="container">
           <h1>Home</h1>
+          {data.categories.map((category) => (
+            <Section key={category.id} name={category.name} />
+          ))}
         </div>
     </div>
   );
