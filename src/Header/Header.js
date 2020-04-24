0d4ff4eb367bb72fa59448ceabc12b7bf93b4c77
LCSmith87 <LCSmith87@gmail.com>
2020-04-23 19:50:01 -0500

    Added react-router-dom and updated header/app

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Header.css'
 import logo from './myreads-logo.png'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 const Header = () => {
 	return(
 		<div className="header">
-			<img src={logo} alt="MyReads Logo" />
+			<div className="container">
+				<Router>
+					<Link to="/"><img src={logo} alt="MyReads Logo" /></Link>
+				</Router>
+			</div>
 		</div>
 	)
 }
