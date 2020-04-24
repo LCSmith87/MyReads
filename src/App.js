64021f666100022cdddeb715b1c741c4da8257f4
LCSmith87 <LCSmith87@gmail.com>
2020-04-24 11:56:00 -0500

    Imported books from server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Section from './Section/Section.js';
+import * as booksAPI from './BooksAPI';
 
 const data = {
   categories: [
@@ -19,19 +20,33 @@ const data = {
   ]
 };
 
-function App() {
-  return (
-    <div className="App">
-        <div className="container">
-          <h1>Home</h1>
-          {data.categories.map((category) => (
-            <Section key={category.id} name={category.name}>
-              <h3>I am a child</h3>
-            </Section>
-          ))}
-        </div>
-    </div>
-  );
+class App extends Component {
+  state = {
+    books: []
+  }
+  componentDidMount() {
+    booksAPI.getAll()
+    .then((books) => {
+      this.setState(() => ({
+        books
+      }))
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+          <div className="container">
+            <h1>Home</h1>
+            {console.log(this.state)}
+            {data.categories.map((category) => (
+              <Section key={category.id} name={category.name}>
+                <h3>I am a child</h3>
+              </Section>
+            ))}
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
